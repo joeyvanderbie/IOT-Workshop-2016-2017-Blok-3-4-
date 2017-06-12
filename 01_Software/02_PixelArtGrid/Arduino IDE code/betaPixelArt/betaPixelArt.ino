e2a2e8edd8c9f1dae771f23596ea38d44758b2d7
YaBoyPete <peter.marcelis@hva.nl>
2017-06-12 19:54:36 +0200

    Pixel Art Beta changes: - 2 request calls to api instead of 3 - code cleanup - removed irrelevant functions
    ps:
    - still require implementation of LED strip

--- 01_Software/02_PixelArtGrid/Arduino IDE code/betaPixelArt/betaPixelArt.ino
+++ 01_Software/02_PixelArtGrid/Arduino IDE code/betaPixelArt/betaPixelArt.ino
@@ -4,7 +4,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiManager.h>
 
-// I should throw all of this into a config file
+// almost all of this should be in a config file [config.h]
 #define DEBUG_MODE
 #define BUTTON_PIN  D1
 #define BUTTONLOW_PIN D0
@@ -13,17 +13,17 @@
 #define CONFIG_SSID "icu"
 #define BACKUP_SSID "VGV75198714B3"
 #define BACKUP_PASSWORD "wNSY4GMMpzPP"
-#define REQUEST_DELAY 500
+#define REQUEST_DELAY 800
 
-boolean isReady = false;
-String internalTimer;
-String externalTimer;
-int intTimer = 0;
-int exTimer = 99999999;
+
+//add variable for ledstrip color || also add variables for brightness & duration?
+boolean isReady = false; // tells the box if it can turn on the ledstrip
+int intTimer = 999999999999; // epoch time + 10 [gets the real epoch time + 10 seconds]
+int exTimer = 0; // will be equal to the realtime epoch time
 
 
 int oldTime = 0;
-String ChipID; // to be later implemented with a change in the api
+String ChipID;
 String serverURL = SERVER_URL;
 OpenWiFi hotspot;
 
@@ -62,25 +62,20 @@ void setup()
   hotspot.begin(BACKUP_SSID, BACKUP_PASSWORD);
 
   ChipID = generateChipID();
-  printDebugMessage(String("Last 2 bytes of chip ID: ") + ChipID);
+  printDebugMessage(String("chipID is: ") + ChipID);
 }
 
 void loop()
 {
-  //Check for button press
 
   //Every requestDelay, send a request to the server
   if (millis() > oldTime + REQUEST_DELAY)
   {
-    if(!isReady){
-    getColor();
-    isSetupReady();
+    if(isReady != true){
+    receiveVariables();
     } else {
-      // is internal epoch == api call to current epoch?
       Serial.println("setup complete");
       isTimerSynced();
-      Serial.println(intTimer);
-      Serial.println(exTimer);
       if(intTimer <= exTimer){
         Serial.println("LIGHTS SHOULD GO ON NOW");
         intTimer = 0;
@@ -102,15 +97,11 @@ void loop()
   return chipIDString;
 }
 
-
-    //ask for color
-    void getColor(){
-      // request color from api for this chip id
+    void receiveVariables(){
       HTTPClient http;
-  String requestString = serverURL + "/api4.php?t=gc&c="+ ChipID; // look up api index, action is 
+  String requestString = serverURL + "/api5.php?t=cnfg&c="+ ChipID +"&o=grdy";
   http.begin(requestString);
   int httpCode = http.GET();
-  
   if (httpCode == 200)
   {
     String response;
@@ -123,69 +114,41 @@ void loop()
     }
     else
     {
+
+       int firstComma = response.indexOf(',');
+       int secondComma = response.indexOf(',', firstComma + 1);
+ 
       // string color = response = gonna be the color you receiver for you chipid
       String color = response.substring(0, 5);
+      String ready = response.substring(firstComma + 1, secondComma);
+      String internalTimer = response.substring(secondComma + 1, response.length());
+      Serial.println(color);
+      Serial.println(ready);
+      Serial.println(internalTimer);
      // save String color into a variable color
-      printDebugMessage(response);
-    }
-  }
-    }
-    
-    //ask if ready
-    void isSetupReady(){
-    // ask the api is there is a value in ready?
-          HTTPClient http;
-  String requestString = serverURL + "/api4.php?t=grdy";
-  http.begin(requestString);
-  int httpCode = http.GET();
-  
-  if (httpCode == 200)
-  {
-    String response;
-    response = http.getString();
-    if (response == "-1")
-    {
-    printDebugMessage("There are no messages waiting in the queue");
-    }
-    else {
-      int firstComma = response.indexOf(',');
-      // timer
-      String ready = response.substring(0, 1); // if 1 it is ready
       if (ready == "1"){
-      internalTimer = response.substring(firstComma + 1, response.length());
-      intTimer = internalTimer.toInt();
-      isReady = true;
+        isReady = true;
       }
-      Serial.println(response);
     }
   }
-    
     }
     
-    // sync timer to server timer
-    void isTimerSynced(){
-      
-            HTTPClient http;
-            // request timer from server
-                  // epochtime save in variable, then in isTimerSynced ask if internal epochtime == server epochtime
-  String requestString = serverURL + "/Pixel_Art_Timer.php";
-  http.begin(requestString);
-  int httpCode = http.GET();
-  
-  if (httpCode == 200)
-  {
-    String response;
-    response = http.getString();
-
-    if (response == "-1")
+    void isTimerSynced()
     {
-      // I can never get here, buggy stuff
-    printDebugMessage("There are no messages waiting in the queue");
-    } else {
-      externalTimer = response.substring(0, response.length());
-      exTimer = externalTimer.toInt();
-    }
-  }
-      
+     HTTPClient http;
+     String requestString = serverURL + "/epochtime.php";
+     http.begin(requestString);
+     int httpCode = http.GET();
+       if (httpCode == 200)
+       {
+        String response;
+        response = http.getString();
+           if (response != "-1")
+           {
+             String externalTimer = response.substring(0, response.length());
+             exTimer = externalTimer.toInt();
+            // Serial.println(exTimer);
+           }
+       }
     }
 
